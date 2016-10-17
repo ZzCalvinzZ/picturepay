74e3ebf64cb0cf7748971fe1cd5cc5d8a3848572
Calvin Collins <calvinkcollins@gmail.com>
2016-10-16 23:55:50 -0400

    more ui changes

--- picture/forms.py
+++ picture/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from picture.models import PaymentNote, Picture
 
+NUMBER_PRESET_OPTIONS = (
+	(5, '5'),
+	(10, '10'),
+	(20, '20'),
+	(0, 'Custom')
+)
+
 class PaymentNoteForm(forms.ModelForm):
+	number_preset = forms.ChoiceField(required=False, choices=NUMBER_PRESET_OPTIONS)
+	number = forms.IntegerField(required=False,widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
 	class Meta:
 		model = PaymentNote
 		fields = ['url', 'name', 'number']
@@ -9,19 +19,26 @@ class PaymentNoteForm(forms.ModelForm):
 		widgets= {
 			'url': forms.TextInput(attrs={'class': 'form-control'}),
 			'name': forms.TextInput(attrs={'class': 'form-control'}),
-			'number': forms.NumberInput(attrs={'class': 'form-control'}),
 		}
 
 	def __init__(self, *args, **kwargs):
 		self.picture = kwargs.pop('picture')
 		return super().__init__(*args, **kwargs)
 
-	def clean_number(self):
+	def clean(self):
+		cleaned_data = super().clean()
 
-		data = self.cleaned_data['number']
+		preset = cleaned_data.get('number_preset', '0') #presets aren't always used
+		number = cleaned_data['number']
 
-		if not self.picture.has_at_least_n_left_uncovered(int(data)):
-			raise forms.ValidationError("Not enough pixels are left to process that request")
+		if not preset or preset == '0':
+			if not number or number <= 0:
+				self.add_error('number', "Please enter a pixel amount!")
 
-		return data
+		else:
+			number = cleaned_data['number'] = int(preset)
 
+			if number and not self.picture.has_at_least_n_left_uncovered(int(number)):
+				self.add_error('number', "Not enough pixels are left to process that request")
+
+		return cleaned_data
--- picture/static/js/main.js
+++ picture/static/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-	$(window).stuck();
+	if ($(window).width() > 752) {
+		$(window).stuck();
+	}
+
+	if ($('.errorlist').length > 0) {
+		$('.open-reveal-form').click();
+	}
 });
+
